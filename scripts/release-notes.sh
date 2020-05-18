005941605919e2e6b781b886df8f0f7a0fb61baa
Sameer Naik <sameer@triggermesh.com>
2020-05-18 21:20:25 +0530

    add circleci pipeline

---
+++ scripts/release-notes.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env sh
+
+RELEASE=${1:-${GIT_TAG}}
+RELEASE=${RELEASE:-${CIRCLE_TAG}}
+
+if [ -z "${RELEASE}" ]; then
+	echo "Usage:"
+	echo "./scripts/release-notes.sh v0.1.0"
+	exit 1
+fi
+
+if ! git rev-list ${RELEASE} >/dev/null 2>&1; then
+	echo "${RELEASE} does not exist"
+	exit
+fi
+
+KCHANNEL="azure-event-channel"
+BASE_URL="https://github.com/triggermesh/${KCHANNEL}/releases/download/${RELEASE}"
+PREV_RELEASE=${PREV_RELEASE:-$(git describe --tags --abbrev=0 ${RELEASE}^ 2>/dev/null)}
+PREV_RELEASE=${PREV_RELEASE:-$(git rev-list --max-parents=0 ${RELEASE}^ 2>/dev/null)}
+NOTABLE_CHANGES=$(git cat-file -p ${RELEASE} | sed '/-----BEGIN PGP SIGNATURE-----/,//d' | tail -n +6)
+CHANGELOG=$(git log --no-merges --pretty=format:'- [%h] %s (%aN)' ${PREV_RELEASE}..${RELEASE})
+if [ $? -ne 0 ]; then
+	echo "Error creating changelog"
+	exit 1
+fi
+
+COMMANDS=$(sed -n -e "s/^\(COMMANDS[[:space:]]*=[[:space:]]*\)\(.*\)$/\2/p" Makefile)
+PLATFORMS=$(sed -n -e "s/^\(TARGETS[[:space:]]*?=[[:space:]]*\)\(.*\)$/\2/p" Makefile)
+RELEASE_ASSETS_TABLE=$(
+  echo -n "|"; for command in ${COMMANDS}; do echo -n " ${command} |"; done ; echo
+  echo -n "|"; for command in ${COMMANDS}; do echo -n "--|"; done ; echo
+  echo -n "|"
+  for command in ${COMMANDS}; do
+    echo -n " ([container](https://gcr.io/triggermesh/${KCHANNEL}-${command}:${RELEASE}))"
+    for platform in ${PLATFORMS}; do
+      echo -n " ([${platform}](${BASE_URL}/${KCHANNEL}-${command}-${platform%/*}-${platform#*/}))"
+    done
+    echo -n " |"
+  done
+  echo
+)
+
+cat <<EOF
+${NOTABLE_CHANGES}
+
+## Installation
+
+Download Triggermesh Knative Azure Events Channel ${RELEASE}
+
+${RELEASE_ASSETS_TABLE}
+
+## Changelog
+
+${CHANGELOG}
+EOF
